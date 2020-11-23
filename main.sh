67625edbb2b39637a2a4c0b399b917aaa7809f81
bud <robstenklippa@gmail.com>
2020-11-23 21:16:47 +0100

    FIX: typo image -> img

--- main.sh
+++ main.sh
@@ -40,7 +40,7 @@ main(){
   elif ((__o[get])); then
     # print everyting inside <msg> tag (set with -s)
     sed -rn 's/^<msg>([^<]+)<.+/\1/p' "$trg_file"
-  elif [[ -z $message${__o[image]}${__o[icon]} ]]; then
+  elif [[ -z $message${__o[img]}${__o[icon]} ]]; then
     # this is the default action that the module itself
     # should get. activated when there are no message.
     # genmonify --module MODULE_NAME
